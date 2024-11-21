b8369348565216225d858947c23a0cd90ec7aba3
caterina-tais <caterinadefranchis@epet12smandes.edu.ar>
2024-11-21 11:32:36 -0300

    webhelados

---
+++ Scripts/master.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cartButtons = document.querySelectorAll('.add-to-cart');
+    cartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Item added to cart!');
+        });
+    });
+});
